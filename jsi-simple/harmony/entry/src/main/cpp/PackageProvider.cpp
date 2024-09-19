5630e166d36dca8345d7e9e739d43ac4e0ff3ceb
Sunbreak <sunbreak.wang@gmail.com>
2024-09-19 15:21:41 +0800

    chore: config harmony

--- jsi-simple/harmony/entry/src/main/cpp/PackageProvider.cpp
+++ jsi-simple/harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,10 +1,10 @@
 #include "RNOH/PackageProvider.h"
-#include "MyModPackage.h"
+#include "JsiSimplePackage.h"
 
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
     return {
-        std::make_shared<MyModPackage>(ctx),
+        std::make_shared<JsiSimplePackage>(ctx),
     };
 }
