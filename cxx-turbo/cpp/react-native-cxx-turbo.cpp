564f1300157dae71929dd96fb580874c29e88e23
Sunbreak <sunbreak.wang@gmail.com>
2024-09-19 11:31:51 +0800

    chore: ArkTSTurboModule to CxxTurboModule

---
+++ cxx-turbo/cpp/react-native-cxx-turbo.cpp
@@ -0,0 +1,5 @@
+#include "react-native-cxx-turbo.h"
+
+double multiply(double a, double b) {
+    return a * b;
+}
--- cxx-turbo/harmony/cxx_turbo/src/main/cpp/CxxTurboPackage.h
+++ cxx-turbo/harmony/cxx_turbo/src/main/cpp/CxxTurboPackage.h
@@ -1,11 +1,25 @@
 #include "RNOH/Package.h"
-#include "RNOH/generated/BaseReactNativeCxxTurboPackage.h"
+#include "CxxTurbo.h"
 
 namespace rnoh {
 
-class CxxTurboPackage : public BaseReactNativeCxxTurboPackage {
+class CxxTurboPackageTurboModuleFactoryDelegate : public TurboModuleFactoryDelegate {
   public:
-    CxxTurboPackage(Package::Context ctx) : BaseReactNativeCxxTurboPackage(ctx) {}
+    SharedTurboModule createTurboModule(Context ctx, const std::string &name) const override {
+        if (name == "CxxTurbo") {
+            return std::make_shared<CxxTurbo>(ctx, name);
+        }
+        return nullptr;
+    };
+};
+
+class CxxTurboPackage : public Package {
+  public:
+    CxxTurboPackage(Package::Context ctx) : Package(ctx) {}
+    
+    std::unique_ptr<TurboModuleFactoryDelegate> createTurboModuleFactoryDelegate() override {
+        return std::make_unique<CxxTurboPackageTurboModuleFactoryDelegate>();
+    }
 };
 
 } // namespace rnoh
